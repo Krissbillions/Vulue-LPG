c66940caddcfcb6c040ea7643fce13a4a1da2ffb
Krissbillions <113177040+Krissbillions@users.noreply.github.com>
2025-09-10 11:30:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    allowedHosts: [
+      "439d0371-c3e1-46f2-ae6d-95e30073f73c-00-1xe06z6n6gp8n.riker.replit.dev", // your host
+      "localhost", // keep localhost for local dev
+    ],
+    host: true, // listen on all addresses
+    port: 5173, // you can change if needed
   },
 });
