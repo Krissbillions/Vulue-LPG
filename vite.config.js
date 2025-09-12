21f0806aaa412dc624286642ec03a4d39692f8a1
Krissbillions <113177040+Krissbillions@users.noreply.github.com>
2025-09-12 08:06:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5000,
+    allowedHosts: [
+      "ab4465d3-fa5d-4445-91ca-a06f23218fdd-00-2pgxry97shazx.worf.replit.dev"
+    ],
   },
 });
